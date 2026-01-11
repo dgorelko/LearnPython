ceaa51af6f016e0562866e02c5ea53556b19db90
Dmitry Gorelko <dmitry.gorelko@gmail.com>
2026-01-11 15:17:49 +0300

    Edit README.md, convert tab to spaces, change .gitignore

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ LearnPython
 ===========
 
 1. Описание
+
+Создание venv
+```bash
+python.exe -m venv .venv 
+```
+
+Обновление версии python в venv
+```bash
+python.exe -m venv upgrade .\.venv\ 
+```
--- game.py
+++ game.py
@@ -7,18 +7,18 @@
 number = 42
 
 while True:
-	answer = input("Введите число: ")
-	if not answer or answer == "exit":
-		break
-	if not answer.isdigit():
-		print("Введите правильное число!")
-		continue
+    answer = input("Введите число: ")
+    if not answer or answer == "exit":
+        break
+    if not answer.isdigit():
+        print("Введите правильное число!")
+        continue
 
-	user_answer = int(answer)
-	if user_answer > number:
-		print("Загаданное число меньше")
-	elif user_answer < number:
-		print("Загаданное число больше")
-	else:
-		print("Совершенно верно!")
-		break
+    user_answer = int(answer)
+    if user_answer > number:
+        print("Загаданное число меньше")
+    elif user_answer < number:
+        print("Загаданное число больше")
+    else:
+        print("Совершенно верно!")
+        break
